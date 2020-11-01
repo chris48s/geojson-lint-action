7b3bb2f812cbb4ede680dc20150033329636f431
chris48s <chris.shaw480@gmail.com>
2020-11-01 12:48:47 +0000

    init

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # geojson-lint-action
+
+```yaml
+name: Geojson Lint
+on: [pull_request]
+
+jobs:
+  build:
+    runs-on: ubuntu-latest
+    steps:
+      - name: Checkout
+        uses: actions/checkout@v2
+      - name: Lint
+        uses: chris48s/geojson-lint-action@master
+        with:
+          github-token: '${{ secrets.GITHUB_TOKEN }}'
+```
---
+++ index.js
@@ -0,0 +1,46 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+const geojsonhint = require("@mapbox/geojsonhint");
+const fs = require("fs");
+
+async function run() {
+  try {
+    const token = core.getInput("github-token", { required: true });
+
+    const { pull_request: pr } = github.context.payload;
+    if (!pr) {
+      throw new Error("Event payload missing `pull_request`");
+    }
+
+    const client = github.getOctokit(token);
+
+    const files = client.pulls.listFiles({
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo,
+      pull_number: pr.number,
+    });
+
+    let allErrors = [];
+    files.forEach(function (file) {
+      if (file.filename.match(/\.geojson$/)) {
+        console.log(`Linting ${file.filename} ...`);
+        const data = fs.readFileSync(file.filename, "utf8");
+        const errors = geojsonhint.hint(data, {
+          noDuplicateMembers: false,
+          precisionWarning: false,
+        });
+        if (errors.length > 0) {
+          allErrors.push({ filename: file.filename, errors: errors });
+        }
+      }
+    });
+    if (allErrors.length > 0) {
+      core.setFailed(JSON.stringify(allErrors, null, 2));
+    }
+
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
